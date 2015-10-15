a995c4bf894ddff2b1bd321058793be19f349532
Tarashish Mishra <sunu0000@gmail.com>
2015-10-15 18:05:57 +0530

    Introduce insert and view modes.
    View mode parses links, emails, Twitter handles etc.

--- js/app.js
+++ js/app.js
@@ -1,7 +1,19 @@
 var localforage = require('localforage');
+var Autolinker = require( 'autolinker' );
+
+var input, overlay, helpdiv;
+var overlayActive, insertModeMessage, viewModeMessage;
 
 window.onload = function() {
-    var input = document.querySelector('#input');
+    input = document.querySelector('#input');
+    overlay = document.querySelector("#overlay");
+    helpdiv = document.querySelector("#help");
+
+    insertModeMessage = 'Currently in `Insert` mode. Press `Esc` to switch to `View` mode.'
+    viewModeMessage = 'Currently in `View` mode. Press `Enter` to switch to `Insert` mode.'
+
+    overlay.onclick = activateInput;
+    window.onkeydown = handleKeyboardInput;
 
     localforage.getItem('savedContent', function(err, value) {
         if (err) {
@@ -10,12 +22,42 @@ window.onload = function() {
             input.value = value || "Hello there! Let's write some notes.";
             input.onkeyup = saveContent;
             input.onblur = saveContent;
+            input.onblur = activateOverlay;
+            activateOverlay();
         };
     });
 };
 
 function saveContent(e) {
-    console.log("Saving content...")
     localforage.setItem('savedContent', e.target.value);
     input.value = e.target.value;
+}
+
+function activateInput() {
+    overlay.style.zIndex = -100;
+    overlayActive = false;
+    helpdiv.innerHTML = insertModeMessage;
+    var temp = input.value;
+    input.value = '';
+    input.focus();
+    input.value = temp;
+}
+
+function activateOverlay() {
+    input.blur();
+    var linkedText = Autolinker.link(input.value);
+    overlay.innerHTML = linkedText;
+    overlay.style.zIndex = 100;
+    overlayActive = true;
+    helpdiv.innerHTML = viewModeMessage;
+}
+
+function handleKeyboardInput(e) {
+    if (e.keyCode===27){
+        activateOverlay();
+    }
+    if ((e.keyCode===13) && (overlayActive==true)){
+        e.preventDefault();
+        activateInput();
+    }
 }
